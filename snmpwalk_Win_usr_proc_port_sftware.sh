f8b094dd284af91ed259ff4e01632a57c371974b
Brian Rohan <BrianjRohan@gmail.com>
2017-07-16 19:49:34 -0700

    Add files via upload

--- snmpwalk_Win_usr_proc_port_sftware.sh
+++ snmpwalk_Win_usr_proc_port_sftware.sh
@@ -4,16 +4,16 @@ if [[ $# -eq 0 ]]; then
 	echo "This script requires a file with IP's to run against"
 fi
 
-for ip in $1
+cat $1 | while read line
 do
-	echo $ip
+	echo $line
 	echo Windows Users;echo
-	snmpwalk -c public $1 -v1 1.3.6.1.4.1.77.1.2.25 | cut -d":" -f2 | cut -d'"' -f2
+	snmpwalk -c public $line -v1 1.3.6.1.4.1.77.1.2.25 | cut -d":" -f2 | cut -d'"' -f2
 	echo;echo Windows Processes;echo
-	snmpwalk -c public $1 -v1 1.3.6.1.2.1.25.4.2.1.2 | cut -d":" -f2 | cut -d'"' -f2
+	snmpwalk -c public $line -v1 1.3.6.1.2.1.25.4.2.1.2 | cut -d":" -f2 | cut -d'"' -f2
 	echo;echo Windows Open TCP Ports;echo
-	snmpwalk -c public $1 -v1 1.3.6.1.2.1.6.13.1.3 | cut -d":" -f2 | cut -d'"' -f2
+	snmpwalk -c public $line -v1 1.3.6.1.2.1.6.13.1.3 | cut -d":" -f2 | cut -d'"' -f2
 	echo;echo Windows Installed Software;echo
-	snmpwalk -c public $1 -v1 1.3.6.1.2.1.25.6.3.1.2
+	snmpwalk -c public $line -v1 1.3.6.1.2.1.25.6.3.1.2
 done
 
